2ea2558fa427282425831a79655ee479c46ff2b6
Vcloud Github <45318961+vcloudgithub@users.noreply.github.com>
2020-06-13 09:27:25 +0530

    response change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res. send("IBM Cloud DevOps is working!");
+	res. send("Looks like IBM Cloud DevOps is working!");
 });
 
 app.listen(8080, () => {
